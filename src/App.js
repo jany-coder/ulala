7e9f7f2ecd037ec738367607e4dcb7d296aee1af
jany-coder <jany_ete@yahoo.com>
2021-09-05 15:22:14 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Nav from './components/shared/nav/Nav';
 function App() {
   return (
     <>
+      <Nav></Nav>
       <h1 className="container">Coming Soon</h1>
     </>
   );
---
+++ src/components/shared/nav/Nav.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from '../../../assets/ulala.png';
+
+const nav = () => {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src={logo} alt=""  height="40" class="d-inline-block align-text-top"/>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Live TV</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Movies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Drama</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <ul class="navbar-nav ms-2">
+                            <li class="nav-item">
+                            <button type="button" class="btn btn-success">Login</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </>
+    );
+};
+
+export default nav;
