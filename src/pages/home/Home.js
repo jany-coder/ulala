19a734d51c1c6df835fc64e3239c2b47c578ae66
Ashab <ashabuddin54321@gmail.com>
2021-09-06 22:00:48 -0700

    carousel init

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BannerCarousel from '../../components/home/bannerCarousel/BannerCarousel';
 import Popular from '../../components/home/popular/Popular';
 import LiveTv from '../../components/liveTv/LiveTv';
 import Footer from '../../components/shared/footer/Footer';
@@ -6,6 +7,7 @@ import Footer from '../../components/shared/footer/Footer';
 const Home = () => {
     return (
         <div className="div">
+            <BannerCarousel />
             <LiveTv />
             <Popular />
             <Footer />
