b3be6c74910a5be7fe6287088a3eead9b5a6369a
jany-coder <jany_ete@yahoo.com>
2021-09-08 03:50:13 +0600

    nav

--- src/components/shared/nav/Nav.js
+++ src/components/shared/nav/Nav.js
@@ -6,7 +6,7 @@ import './_nav.scss';
 const nav = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">
                         <img src={logo} alt="" height="40" className="d-inline-block align-text-top" />
