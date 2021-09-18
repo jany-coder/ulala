8effebf660fcacc791189982e9c1c78ff41be614
jany-coder <jany_ete@yahoo.com>
2021-09-19 01:26:30 +0600

    firebase auth change

--- src/components/login/firebase.config.js
+++ src/components/login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDhVRRSZXa07iXm71TS33pwlTvgIjJE_eg",
-    authDomain: "ulala-4d316.firebaseapp.com",
-    projectId: "ulala-4d316",
-    storageBucket: "ulala-4d316.appspot.com",
-    messagingSenderId: "655459899951",
-    appId: "1:655459899951:web:5f298da0de2e5321a184da"
-  };
+  apiKey: "AIzaSyBuv3U7MEP4cqCwKoKjQ2JRSyvss5ULi0M",
+  authDomain: "ulala-b38b0.firebaseapp.com",
+  projectId: "ulala-b38b0",
+  storageBucket: "ulala-b38b0.appspot.com",
+  messagingSenderId: "358344082476",
+  appId: "1:358344082476:web:eca6ec279b82f40f28bd89"
+};
 
 export default firebaseConfig;
 
