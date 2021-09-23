3ba5bd42a0c2f24a8a1baf0ed7b54f168f7cd1b4
jany-coder <jany_ete@yahoo.com>
2021-09-23 16:16:42 +0600

    dl

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,10 +2,6 @@ import React from 'react';
 import BannerCarousel from '../../components/mainPage/bannerCarousel/BannerCarousel';
 import Popular from '../../components/mainPage/popular/Popular';
 import Faq from '../../components/mainPage/faq/Faq';
-<<<<<<< HEAD
-import Footer from './../../components/shared/footer/Footer';
-=======
->>>>>>> 2daf269ceaee482306b55a0b1d21838ccb469a2e
 import Natok from './../../components/NatokData/Natok';
 import Subscription from '../../components/mainPage/Subscription';
 import Movie from '../../components/movie/Movie';
