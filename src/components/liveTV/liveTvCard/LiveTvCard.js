64f7def922a0eb383b5ddd50986477e19cdfefaf
jany-coder <jany_ete@yahoo.com>
2021-09-23 15:53:52 +0600

    changes

--- src/components/liveTV/liveTvCard/LiveTvCard.js
+++ src/components/liveTV/liveTvCard/LiveTvCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './_liveTvCard.scss';
+import '../../shared/commonCSS/_commonCSS.scss';
 
 const LiveTvCard = (props) => {
 
@@ -8,7 +9,7 @@ const LiveTvCard = (props) => {
     return (
         <>
            <div onClick={handlePlayVideo} className="text-center">
-               <div  className="tv-card-img shadow d-flex justify-content-center align-items-center">
+               <div  className="img-hover-zoom tv-card-img shadow d-flex justify-content-center align-items-center">
                     <img src={img} alt="" />
                </div>
                <div className="text-cente">
